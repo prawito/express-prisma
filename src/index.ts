2e2f89e538ec77a77a62feebb7ecda513ea1299e
Prawito Hudoro <prawitohudoro@gmail.com>
2023-08-24 19:00:07 +0700

    hashing password dengan bcrypt

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const PORT = 5000;
 const prisma = new PrismaClient();
 
 app.use(express.json())
+
+// REGISTER
+app.use('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await prisma.users.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    res.json({
+        message: 'user created'
+    })
+})
+
+// LOGIN
+app.use('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await prisma.users.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+    if(!user) {
+        return res.status(404).json({
+            message: 'User not found'
+        })
+    }
+
+    if(!user.password) {
+        return res.status(404).json({
+            message: 'Password not set'
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(isPasswordValid){
+        return res.json({
+            data: {
+                id: user.id,
+                name: user.name,
+                address: user.address
+            }
+        })
+    } else {
+        return res.status(403).json({
+            message: 'Wrong password'
+        })
+    }
+})
 
 // CREATE
 app.post('/users', async (req, res, next) => {
