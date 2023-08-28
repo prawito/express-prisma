2f53d2110a20d6c07f70077cdc3b508028421896
Prawito Hudoro <prawitohudoro@gmail.com>
2023-08-28 08:49:57 +0700

    add json web token

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const app = express();
 const PORT = 5000;
 const prisma = new PrismaClient();
 
 app.use(express.json())
+
+interface UserData {
+    id: string;
+    name: string;
+    address: string;
+}
+
+interface ValidationRequest extends Request {
+    userData: UserData
+}
+
+const accessValidation = (req: Request, res: Response, next: NextFunction) => {
+    const validationReq = req as ValidationRequest
+    const {authorization} = validationReq.headers;
+
+    console.log('here: ', authorization)
+
+    if(!authorization){
+        return res.status(401).json({
+            message: 'Token diperlukan'
+        })
+    }
+
+    const token = authorization.split(' ')[1];
+    const secret = process.env.JWT_SECRET!;
+
+    try {
+        const jwtDecode = jwt.verify(token, secret);
+
+        if(typeof jwtDecode !== 'string'){
+            validationReq.userData = jwtDecode as UserData
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+    next()
+}
 
 // REGISTER
 app.use('/register', async (req, res) => {
@@ -51,12 +91,25 @@ app.use('/login', async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if(isPasswordValid){
+        const payload = {
+            id: user.id,
+            name: user.name,
+            address: user.address
+        }
+
+        const secret = process.env.JWT_SECRET!;
+
+        const expiresIn = 60 * 60 * 1;
+
+        const token = jwt.sign(payload, secret, {expiresIn: expiresIn})
+
         return res.json({
             data: {
                 id: user.id,
                 name: user.name,
                 address: user.address
-            }
+            },
+            token: token
         })
     } else {
         return res.status(403).json({
@@ -66,7 +119,7 @@ app.use('/login', async (req, res) => {
 })
 
 // CREATE
-app.post('/users', async (req, res, next) => {
+app.post('/users', accessValidation, async (req, res, next) => {
     const {name, email, address} = req.body;
 
     const result = await prisma.users.create({
@@ -83,7 +136,7 @@ app.post('/users', async (req, res, next) => {
 })
 
 // READ
-app.get('/users', async (req, res) => {
+app.get('/users',accessValidation, async (req, res) => {
     const result = await prisma.users.findMany({
         select: {
             id: true,
@@ -99,7 +152,7 @@ app.get('/users', async (req, res) => {
 })
 
 // UPDATE
-app.patch('/users/:id', async (req, res) => {
+app.patch('/users/:id', accessValidation, async (req, res) => {
     const {id} = req.params
     const {name, email, address} = req.body
     
@@ -120,7 +173,7 @@ app.patch('/users/:id', async (req, res) => {
 })
 
 // DELETE
-app.delete('/users/:id', async (req, res) => {
+app.delete('/users/:id', accessValidation, async (req, res) => {
     const {id} = req.params;
 
     const result = await prisma.users.delete({
