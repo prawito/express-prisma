521af0cefa25b381810c4bf3a846e99ef8de05d1
Prawito Hudoro <prawitohudoro@gmail.com>
2023-09-21 09:05:12 +0700

    Google Auth

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,29 @@ import express, { NextFunction, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import {google} from 'googleapis';
 
 const app = express();
 const PORT = 5000;
 const prisma = new PrismaClient();
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    'http://localhost:5000/auth/google/callback'
+);
+
+const scopes = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+const authorizationUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+    include_granted_scopes: true,
+})
 
 app.use(express.json())
 
@@ -47,6 +66,73 @@ const accessValidation = (req: Request, res: Response, next: NextFunction) => {
     }
     next()
 }
+
+// GOOGLE Login
+app.get('/auth/google', (req, res) => {
+    res.redirect(authorizationUrl);
+})
+
+// GOOGLE callback login
+app.get('/auth/google/callback', async (req, res) => {
+    const {code} = req.query
+
+    const {tokens} = await oauth2Client.getToken(code as string);
+
+    oauth2Client.setCredentials(tokens);
+
+    const oauth2 = google.oauth2({
+        auth: oauth2Client,
+        version: 'v2'
+    })
+
+    const {data} = await oauth2.userinfo.get();
+
+    if(!data.email || !data.name){
+        return res.json({
+            data: data,
+        })
+    }
+
+    let user = await prisma.users.findUnique({
+        where: {
+            email: data.email
+        }
+    })
+
+    if(!user){
+        user = await prisma.users.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                address: "-"
+            }
+        })
+    }
+
+    const payload = {
+        id: user?.id,
+        name: user?.name,
+        address: user?.address
+    }
+
+    const secret = process.env.JWT_SECRET!;
+
+    const expiresIn = 60 * 60 * 1;
+
+    const token = jwt.sign(payload, secret, {expiresIn: expiresIn})
+
+    // return res.redirect(`http://localhost:3000/auth-success?token=${token}`)
+
+    return res.json({
+        data: {
+            id: user.id,
+            name: user.name,
+            address: user.address
+        },
+        token: token
+    })
+
+})
 
 // REGISTER
 app.use('/register', async (req, res) => {
